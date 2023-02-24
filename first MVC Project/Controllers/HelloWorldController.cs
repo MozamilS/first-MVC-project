0bf88a5a34e03490fb19b08265e6b2277d2ee121
Mozamil Sadat <mozamilsadat@Mozamils-MacBook-Air.local>
2023-02-24 12:32:56 +0430

    Hello world controller added

---
+++ first MVC Project/Controllers/HelloWorldController.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace first_MVC_Project.Controllers
+{
+    public class helloWorldController : Controller
+    {
+        public string Index()
+        {
+            return "This is the default page";
+        }
+
+        public string Hello()
+        {
+            return "Who's there?";
+        }
+    }
+}
